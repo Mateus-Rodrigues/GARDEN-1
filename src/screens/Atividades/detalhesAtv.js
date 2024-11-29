859fd4fccd6bd3e3b9f688814b520ac8254ab591
MATEUS APARECIDO PESSOA RODR <mateus.rodrigues64@etectupa.local>
2024-11-28 22:50:25 -0300

    salve
    dfs

--- src/screens/Atividades/detalhesAtv.js
+++ src/screens/Atividades/detalhesAtv.js
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
-        backgroundColor: '#fff',
+        // backgroundColor: '#fff',
       },
       title: {
         fontSize: 24,
